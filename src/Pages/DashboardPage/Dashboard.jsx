241127bbbb987e10c93c7e29f8f9cfb912a36932
PENN JR <pennjunior680@gmail.com>
2023-03-21 15:25:21 +0100

    Working on dashboard

--- src/Pages/DashboardPage/Dashboard.jsx
+++ src/Pages/DashboardPage/Dashboard.jsx
@@ -1,22 +1,54 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getCurrentUser } from '../../api/auth';
 import './Dashboard.css';
 
 function Dashboard() {
   const [user, setUser] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getCurrentUser().then(setUser);
   }, []);
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/login', { replace: true });
+    window.location.reload(true);
+  };
 
   return (
     <div className="dash">
-      <h1 className="head">WELCOME TO YOUR DASHBOARD</h1>
-      <p className="head">
-        {user?.first_name} {user?.last_name}
-        <Link to="/edith-profile">Edith-Profile</Link>
-      </p>
+      <div>
+        <div className="sidebarz">
+          <header>User Panel</header>
+          <ul>
+            <li>
+              <a href="dashboard">Dashboard</a>
+            </li>
+            <li>
+              <Link to="/edith-profile">Edith Profile</Link>
+            </li>
+            <li>
+              <a href="jin">Settings</a>
+            </li>
+            <li>
+              <a href="beer">Documentation</a>
+            </li>
+            <li>
+              <button className="logoutz" type="button" onClick={logout}>
+                Log Out
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1 className="head">WELCOME TO YOUR DASHBOARD</h1>
+        <p className="head">
+          {user?.first_name} {user?.last_name}
+        </p>
+      </div>
     </div>
   );
 }
